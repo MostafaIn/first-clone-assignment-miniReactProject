40519f6ff809bd6a79db191388c8cf6e1ae8f46f
MostafaIn <mostafa.hazareh@integrify.fi>
2019-04-28 23:07:34 +0300

    Updates

---
+++ precache-manifest.eab7df57ff912177c04529478e098a0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c8571e07ea7d99aa99d3b14d46f5923",
+    "url": "/first-clone-assignment-miniReactProject/index.html"
+  },
+  {
+    "revision": "fb7a5ac435ba625c2c96",
+    "url": "/first-clone-assignment-miniReactProject/static/css/main.fc41f8ac.chunk.css"
+  },
+  {
+    "revision": "c139c7d5d96977a38798",
+    "url": "/first-clone-assignment-miniReactProject/static/js/2.2b27d59f.chunk.js"
+  },
+  {
+    "revision": "fb7a5ac435ba625c2c96",
+    "url": "/first-clone-assignment-miniReactProject/static/js/main.fbc6f282.chunk.js"
+  },
+  {
+    "revision": "39a16675d6a21bb25ee1",
+    "url": "/first-clone-assignment-miniReactProject/static/js/runtime~main.19621c48.js"
+  }
+]);
